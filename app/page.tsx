7f7f8c35971e024a76e464bf561d4003dba29102
Georgenrg <george.nrg@gmail.com>
2025-04-27 19:44:13 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
           .map((profile, index) => (
             <motion.div
   key={index}
-  className="p-4 border rounded-lg shadow cursor-pointer hover:shadow-lg hover:-translate-y-2 transition-all"
+  className="p-4 border rounded-lg shadow cursor-pointer hover:shadow-lg hover:-translate-y-2 transition-all flex flex-col justify-between h-56"
   initial={{ opacity: 0, y: 20 }}
   animate={{ opacity: 1, y: 0 }}
   transition={{ duration: 0.5, delay: index * 0.1 }}
